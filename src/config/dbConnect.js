aabe2b8197c5575c96f0dd0b608601427440df12
PeuGomes <pedroivobarretogomes26@gmail.com>
2022-07-05 23:17:27 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://Pedro:XlYd5jqmeBJRcnN5@cluster0.qiqfo.mongodb.net/Pedro-node");
+mongoose.connect("mongodb+srv://<password>@cluster0.qiqfo.mongodb.net/Pedro-node");
 
 let db = mongoose.connection;
 
